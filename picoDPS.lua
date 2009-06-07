0f08b928984a07ef238b2a74efff1933f8a452d8
Tekkub <tekkub@gmail.com>
2009-06-06 19:43:37 -0600

    Clean up GUID management a bit

--- picoDPS.lua
+++ picoDPS.lua
@@ -1,5 +1,5 @@
 
-local myDamage, timeincombat, pId, petId = 0, 0
+local myDamage, timeincombat, pId = 0, 0
 local damagetotals, times, ids, units, unitnames, colors, shown = {}, {}, {}, {}, {}, {}
 local events = {SWING_DAMAGE = true, RANGE_DAMAGE = true, SPELL_DAMAGE = true, SPELL_PERIODIC_DAMAGE = true, DAMAGE_SHIELD = true, DAMAGE_SPLIT = true}
 for class,color in pairs(RAID_CLASS_COLORS) do colors[class] = string.format("%02x%02x%02x", color.r*255, color.g*255, color.b*255) end
@@ -18,21 +18,19 @@ f:SetScript("OnUpdate", function(self, elap)
 		if UnitAffectingCombat(unit) then times[id] = (times[id] or 0) + elap end
 	end
 
-	obj.text = string.format("%.1f DPS", (damagetotals[ids.player] or 0)/(times[ids.player] or 1))
+	obj.text = string.format("%.1f DPS", (damagetotals[pId] or 0)/(times[pId] or 1))
 	if shown then obj.OnEnter(shown) end
 end)
 
 
 function f:PLAYER_LOGIN()
 	pId = UnitGUID("player")
-	petId = UnitGUID("pet") or "0x0"
 
-	ids.player, ids.pet = UnitGUID("player"), UnitGUID("pet")
-	units[ids.player] = ids.player
-	unitnames.player = "|cff"..colors[select(2, UnitClass("player"))]..UnitName("player").."|r"
+	units[pId] = pId
+	unitnames[pId] = "|cff"..colors[select(2, UnitClass("player"))]..UnitName("player").."|r"
 
 	local petid = UnitGUID("pet")
-	if petid then units[petid] = ids.player end
+	if petid then units[petid], ids.pet = pId, petid end
 
 	self:PARTY_MEMBERS_CHANGED()
 	self:RAID_ROSTER_UPDATE()
@@ -52,7 +50,7 @@ function f:UNIT_PET(unit)
 	if unit == "player" then
 		local petid = UnitGUID("pet")
 		ids.pet = petid
-		if petid then units[petid] = ids.player end
+		if petid then units[petid] = pId end
 	elseif unit ~= "target" and unit ~= "focus" then
 		local group, id = unit:match("^(%D+)(%d+)$")
 		if not id then
@@ -67,28 +65,30 @@ end
 
 function f:PARTY_MEMBERS_CHANGED()
 	for i=1,4 do
-		ids["party"..i] = UnitGUID("party"..i)
+		local id = UnitGUID("party"..i)
+		ids["party"..i] = id
 		if ids["party"..i] then
-			units[ids["party"..i]] = ids["party"..i]
-			unitnames["party"..i] = "|cff"..colors[select(2, UnitClass("party"..i)) or "PRIEST"]..UnitName("party"..i).."|r"
+			units[id] = id
+			unitnames[id] = "|cff"..colors[select(2, UnitClass("party"..i)) or "PRIEST"]..UnitName("party"..i).."|r"
 		end
 
 		local petid = UnitGUID("partypet"..i)
-		if petid then units[petid] = ids["party"..i] end
+		if petid then units[petid] = id end
 	end
 end
 
 
 function f:RAID_ROSTER_UPDATE()
 	for i=1,40 do
-		ids["raid"..i] = UnitGUID("raid"..i)
+		local id = UnitGUID("raid"..i)
+		ids["raid"..i] = id
 		if ids["raid"..i] then
-			units[ids["raid"..i]] = ids["raid"..i]
-			unitnames["raid"..i] = "|cff"..colors[select(2, UnitClass("raid"..i)) or "PRIEST"]..UnitName("raid"..i).."|r"
+			units[id] = id
+			unitnames[id] = "|cff"..colors[select(2, UnitClass("raid"..i)) or "PRIEST"]..UnitName("raid"..i).."|r"
 		end
 
 		local petid = UnitGUID("raidpet"..i)
-		if petid then units[petid] = ids["raid"..i] end
+		if petid then units[petid] = id end
 	end
 end
 
@@ -96,7 +96,7 @@ end
 function f:COMBAT_LOG_EVENT_UNFILTERED(_, eventtype, id, _, _, _, _, _, spellid, _, _, damage)
 	if not events[eventtype] then return end
 
-	if id == pId or id == petId then
+	if id == pId or id == ids.pet then
 		if eventtype == "SWING_DAMAGE" then
 			damage = spellid
 		end
@@ -108,7 +108,7 @@ end
 
 
 function f:PLAYER_REGEN_ENABLED()
-	obj.text = string.format("%.1f DPS", (damagetotals[ids.player] or 0)/(times[ids.player] or 1))
+	obj.text = string.format("%.1f DPS", (damagetotals[pId] or 0)/(times[pId] or 1))
 end
 
 
@@ -142,7 +142,7 @@ function obj.OnEnter(self)
 	tip:AddLine(" ")
 
 	tip:AddMultiLine("Player", "Total", "DPS")
-	tip:AddMultiLine(unitnames.player or UnitName("player"), damagetotals[ids.player] or 0, string.format("%.1f", (damagetotals[ids.player] or 0)/(times[ids.player] or 1)), nil,nil,nil, 1,1,1, 1,1,1)
+	tip:AddMultiLine(unitnames[pId] or UnitName("player"), damagetotals[pId] or 0, string.format("%.1f", (damagetotals[pId] or 0)/(times[pId] or 1)), nil,nil,nil, 1,1,1, 1,1,1)
 	for id in pairs(damagetotals) do
 		if id ~= pId then tip:AddMultiLine(unitnames[id] or "???", damagetotals[id] or 0, string.format("%.1f", (damagetotals[id] or 0)/(times[id] or 1)), nil,nil,nil, 1,1,1, 1,1,1) end
 	end
