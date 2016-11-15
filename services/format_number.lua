d1ddfc62cc6dd0b655b8336cc1e7bdcca03a4123
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-11-15 02:12:50 -0700

    Also handle numbers in the millions

--- services/format_number.lua
+++ services/format_number.lua
@@ -3,8 +3,10 @@ local myname, ns = ...
 
 
 function ns.FormatNumber(num)
-	if num >= 1000 then
-		return string.format("%.1fk", num/1000.0)
+	if num >= 1000000 then
+		return string.format("%.1fm", num/1000000)
+	elseif num >= 1000 then
+		return string.format("%.1fk", num/1000)
 	else
 		return string.format("%.1f", num)
 	end
